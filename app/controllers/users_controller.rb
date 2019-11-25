d264c01dd750cf13cea94ca8922915f1e6d4d405
EC2 Default User <ec2-user@ip-172-31-34-153.ap-northeast-1.compute.internal>
2019-11-25 18:28:31 +0000

    assets周り修正その２

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
       if params[:image_name]
         @user.image_name = "#{@user.id}.jpg"
         image = params[:image_name]
-        File.binwrite("public/user_images/#{@user.image_name}",image.read)
+        File.binwrite("public/assets/user_images/#{@user.image_name}",image.read)
       end
     
      if @user.save
