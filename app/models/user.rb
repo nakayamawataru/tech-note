1e7720831d80f957f689f4d5f64686b7071f2fb2
EC2 Default User <ec2-user@ip-172-31-36-169.ap-northeast-1.compute.internal>
2019-12-24 14:54:41 +0000

    index整理/最新投稿・登録者表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name,{uniqueness: true,presence:true}
     validates :mail,{presence: true}
     validates :mail,uniqueness: { case_sensitive: false } #一意性の検証（大文字小文字区別しない）
-    validates :password,{presence: true}
+    validates :password,{presence: true,allow_nil: true}
     has_secure_password
     
     def posts
